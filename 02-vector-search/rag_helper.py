dea10093dea17162e61032fba1c4d5d6514d1ce2
Cinnaminh-toast <ncaminh.1998@gmail.com>
2026-06-20 07:21:45 +0000

    08-pgvector

--- 02-vector-search/rag_helper.py
+++ 02-vector-search/rag_helper.py
@@ -94,3 +94,34 @@ class RAGVector(RAGBase):
             filter_dict=filter_dict,
             num_results=num_results
         )
+
+class RAGPgVector(RAGBase):
+    def __init__(self, conn, embedder, **kwargs):
+        super().__init__(index=None, **kwargs)
+        self.embedder = embedder
+        self.conn = conn
+
+    def search(self, query, num_results=5):
+        query_vector = self.embedder.encode(query)
+        query_str = "[" + ",".join(str(x) for x in query_vector) + "]"
+
+        try:
+            rows = self.conn.execute(
+                """
+                SELECT course, section, question, answer
+                FROM documents
+                WHERE course = %s
+                ORDER BY embedding <=> %s::vector
+                LIMIT %s
+                """,
+                (self.course, query_str, num_results)
+            ).fetchall()
+        except Exception as e:
+            print(e)
+            conn.rollback()
+            return None
+
+        return [
+            {"course": r[0], "section": r[1], "question": r[2], "answer": r[3]}
+            for r in rows
+        ]
